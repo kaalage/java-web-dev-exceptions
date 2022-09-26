e82addc27ad5d919f5e6c76a7bc147eebe5df925
Karina Lage <karinalage@Karinas-MacBook-Air.local>
2022-09-26 11:26:00 -0500

    Finished exercises Class 9 - Exceptions

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -2,12 +2,14 @@ package org.launchcode.exercises;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        //Divide(5,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +18,46 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (String file : studentFiles.values()) {
+            CheckFileExtension(file);
+        }
+
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static int Divide(int x, int y) {
         // Write code here!
+
+        if (y <= 0) {
+            try {
+                throw new ArithmeticException("Can't divide by 0");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return x/y;
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
+        int points = 0;
+        if (fileName.isEmpty() || fileName == null) {
+            try {
+                throw new RuntimeException("Field can't be empty");
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                return points = -1;
+            }
+        } else if (fileName.endsWith(".java")) {
+                return points = 1;
+        } else if (!fileName.endsWith(".java")) {
+                return points = 0;
+        }
+
+        return points;
     }
 
 }
+
+
